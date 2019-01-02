cc48210d596da4bee963b5db3cfbe86e702bff8d
cckn <cckn1985@gmail.com>
2019-01-02 13:35:47 +0900

    add hex2ascii

--- src/utils.ts
+++ src/utils.ts
@@ -5,8 +5,19 @@ export class Utils {
      * hex2ascii
      */
     public hex2ascii(value: number, size: number): number[] {
-        return [0, 0]
+        const arr = []
+        const str = value.toString(16)
+        for (let i = 0, l = str.length; i < l; i++) {
+            const hex = Number(str.charCodeAt(i)) // .toString(16)
+            arr.push(hex)
+        }
+        return arr // .join('')
     }
 
     public ascii2hex() {}
+}
+
+if (require.main === module) {
+    const utils = new Utils()
+    console.log(utils.hex2ascii(0x0f, 1))
 }
--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,10 +1,18 @@
-import { expect } from "chai"
-import { Utils } from "../src/utils"
+import { expect } from 'chai'
+import { Utils } from '../src/utils'
 
-describe("Hex to Ascii", () => {
-    it("value가 정확히 변경되는지 확인", () => {
+describe('Hex to Ascii', () => {
+    it('value가 정확히 변경되는지 확인', () => {
         const utils = new Utils()
-        expect(utils.hex2ascii(1, 1)).deep.equal([0x01])
-        expect(utils.hex2ascii(255, 1)).deep.equal([0xff])
+        expect(utils.hex2ascii(0x01, 1)).deep.equal([0x31])
+        expect(utils.hex2ascii(0x09, 1)).deep.equal([0x39])
+        expect(utils.hex2ascii(0x0f, 1)).deep.equal([0x66])
+    })
+
+    it('value가 정확히 변경되는지 확인', () => {
+        const utils = new Utils()
+        expect(utils.hex2ascii(0x01, 1)).deep.equal([0x31])
+        expect(utils.hex2ascii(0x09, 1)).deep.equal([0x39])
+        expect(utils.hex2ascii(0x0f, 1)).deep.equal([0x66])
     })
 })
