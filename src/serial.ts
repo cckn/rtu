78f3bcd874d6f62d22ea8a466fa18f39e47b05df
cckn <cckn1985@gmail.com>
2019-01-03 16:07:08 +0900

    serial 모듈화

--- src/serial.ts
+++ src/serial.ts
@@ -1,19 +1,21 @@
 import SerialPort = require('serialport')
 import { HexPowerInverter } from './hexpower'
 
-const hp = new HexPowerInverter(1)
-console.log(hp.makeFrame(0x52, 0x20, 0x02))
-
-const port = new SerialPort('COM3')
-
-port.write(hp.makeFrame(0x52, 0x20, 0x02), (err: any) => {
-    if (err) {
-        return console.log('Error on write: ', err.message)
+export class Serial {
+    private serial: SerialPort
+    constructor(private port: string) {
+        this.serial = new SerialPort(this.port)
+        this.serial.on('error', (err: any) => {
+            console.log('Error: ', err.message)
+        })
     }
-    console.log('message written')
-})
 
-// Open errors will be emitted as an error event
-port.on('error', (err: any) => {
-    console.log('Error: ', err.message)
-})
+    public write(msg: number[]) {
+        this.serial.write(msg, (err: any) => {
+            if (err) {
+                return console.log('Error on write: ', err.message)
+            }
+            console.log('message written')
+        })
+    }
+}
